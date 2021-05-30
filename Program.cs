e7c752fc6deb2b77447a24f023756d62f8c6665b
Thien Trinh <thientd87@gmail.com>
2021-05-30 12:03:15 +0700

    Parallel programming demo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Parallel_Async_Programing
 {
@@ -7,6 +9,26 @@ namespace Parallel_Async_Programing
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine($"Thread Count:{Thread.GetCurrentProcessorId()}");
+            ParallelFor();
+
+            Console.WriteLine("Press any key ..."); 
+            Console.ReadKey();
+        }
+
+        public static void PrintInfor(string infor){
+            Console.WriteLine($"{infor} task:{Task.CurrentId} thread:{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public static async void RunTask(int i){
+            PrintInfor($"Start task {i}");
+            await Task.Delay(1000);
+            PrintInfor($"Finish task{i}");
+        }
+
+        public static void ParallelFor(){
+            ParallelLoopResult result = Parallel.For(1, 20, RunTask);   // Vòng lặp tạo ra 20 lần chạy RunTask
+            Console.WriteLine($"All task start and finish: {result.IsCompleted}");
         }
     }
 }

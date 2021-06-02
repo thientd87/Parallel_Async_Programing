1896fa3dae5a644bac59bb994743488dc4798913
Thien Trinh <thientd87@gmail.com>
2021-06-02 15:16:13 +0700

    add some way to test async

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Parallel_Async_Programing
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine($"Thread Count:{Thread.GetCurrentProcessorId()}");
+         
             ParallelFor();
 
             Console.WriteLine("Press any key ..."); 
@@ -23,12 +23,31 @@ namespace Parallel_Async_Programing
         public static async void RunTask(int i){
             PrintInfor($"Start task {i}");
             await Task.Delay(1000);
-            PrintInfor($"Finish task{i}");
+            PrintInfor($"Finish task {i}");
         }
 
         public static void ParallelFor(){
+            Console.WriteLine("Start testing Parallel.");
             ParallelLoopResult result = Parallel.For(1, 20, RunTask);   // Vòng lặp tạo ra 20 lần chạy RunTask
-            Console.WriteLine($"All task start and finish: {result.IsCompleted}");
-        }
+            Console.WriteLine($"All parallel task start and finish: {result.IsCompleted}");
+            
+            Console.ReadLine();
+            Console.WriteLine("Start testing Task Run in Loop.");
+            for(int a= 0; a <= 20; a++){
+               Task.Run(() => RunTask(a));
+            }
+            Console.WriteLine("End testing Task Run in Loop.");
+            Console.ReadLine();
+            Console.WriteLine("Start testing Loop in task run.");
+            Task.Run(() => {
+                for(int a= 0; a <= 20; a++){
+                    RunTask(a);
+                }             
+            });
+            
+            Console.WriteLine("End testing Loop in task rung.");
+            
+        }ß
     }
 }
+   

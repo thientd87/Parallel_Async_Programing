f6b2aeffee3e98e0b8fd26414c6dab40e06de9e2
Thien Trinh <thientd87@gmail.com>
2021-06-03 16:34:45 +0700

    Refactor Paralle Test

--- Parallel_Test.cs
+++ Parallel_Test.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,36 +11,32 @@ namespace Parallel_Async_Programing
         public Parallel_Test(){
 
         }
-      public static void PrintInfor(string infor){
-            Console.WriteLine($"{infor} task:{Task.CurrentId} thread:{Thread.CurrentThread.ManagedThreadId}");
+      public static void PrintInfor(string infor, ConsoleColor color){
+          lock(Console.Out){
+              Console.ForegroundColor = color;
+              Console.WriteLine($"{infor} task:{Task.CurrentId} thread:{Thread.CurrentThread.ManagedThreadId}");
+              Console.ResetColor();
+          }
+            
         }
 
-        public static async void RunTask(int i){
-            PrintInfor($"Start task {i}");
+        public static async void RunTask(int i, ConsoleColor color){
             await Task.Delay(1000);
-            PrintInfor($"Finish task {i}");
+            PrintInfor($"Start task {i}", color);
+            await Task.Delay(1000);
+            PrintInfor($"Finish task {i}", color);
         }
 
         public static void ParallelFor(){
             Console.WriteLine("Start testing Parallel.");
-            ParallelLoopResult result = Parallel.For(1, 20, RunTask);   // Vòng lặp tạo ra 20 lần chạy RunTask
+            ParallelLoopResult result = Parallel.For(1, 10, 
+                i => {                    
+                    RunTask(i,(ConsoleColor)new Random().Next(1,10));
+                }                
+            );   // Vòng lặp tạo ra 20 lần chạy RunTask
             Console.WriteLine($"All parallel task start and finish: {result.IsCompleted}");
             
             Console.ReadLine();
-            Console.WriteLine("Start testing Task Run in Loop.");
-            for(int a= 0; a <= 20; a++){
-               Task.Run(() => RunTask(a));
-            }
-            Console.WriteLine("End testing Task Run in Loop.");
-            Console.ReadLine();
-            Console.WriteLine("Start testing Loop in task run.");
-            Task.Run(() => {
-                for(int a= 0; a <= 20; a++){
-                    RunTask(a);
-                }             
-            });
-            
-            Console.WriteLine("End testing Loop in task rung.");
             
         }
     }
